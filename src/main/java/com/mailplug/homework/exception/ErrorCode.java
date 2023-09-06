393b66de03da0a6d7d9383646f7fd497772eeab3
chojunho <whwnsgh1229@naver.com>
2023-09-06 17:01:33 +0900

    UPDATE : Exception 수정 - 중복된 주석 제거, ErrorResponse에 @AllArgsConstructor와 @Builder 어노테이션을 추가하여 더 간결하게 객체를 생성, 예외 처리 로직 중 .status() 와 .body() 메서드 한줄로 축약

--- src/main/java/com/mailplug/homework/exception/ErrorCode.java
+++ src/main/java/com/mailplug/homework/exception/ErrorCode.java
@@ -1,53 +1,20 @@
 package com.mailplug.homework.exception;
 
 import org.springframework.http.HttpStatus;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-
-    /*
-     * 404 NOT_FOUND: 리소스를 찾을 수 없음
-     */
     POST_DELETED_OR_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글이 삭제되었거나 존재하지 않습니다."),
-
-    /*
-     * 400 BAD_REQUEST: 잘못된 입력
-     */
     INVALID_INPUT(HttpStatus.BAD_REQUEST, "잘못된 입력입니다."),
-
-
-    /*
-     * 403 FORBIDDEN: 권한 없음
-     */
     FORBIDDEN(HttpStatus.FORBIDDEN, "권한이 없습니다."),
-
-    /*
-     * 400 BAD_REQUEST: 잘못된 요청
-     */
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
-
-    /*
-     * 404 NOT_FOUND: 리소스를 찾을 수 없음
-     */
     POSTS_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글 정보를 찾을 수 없습니다."),
-
-    /*
-     * 405 METHOD_NOT_ALLOWED: 허용되지 않은 Request Method 호출
-     */
     METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "허용되지 않은 메서드입니다."),
-
-    /*
-     * 500 INTERNAL_SERVER_ERROR: 내부 서버 오류
-     */
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류입니다."),
-
-    ;
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류입니다.");
 
     private final HttpStatus status;
     private final String message;
-
 }
